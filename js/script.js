1f1b5a72e7ce1947c323bcd7f3125498ddfeabc2
Megan <87830907+MeganWallace@users.noreply.github.com>
2023-09-19 21:03:55 -0400

    set up API to fetch images

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
-const button = document.querySelector("button");
-const authorSpan = document.querySelector(".author");
-const imgDiv = document.querySelector(".image-container");
-const img = document.querySelector(".img");
+const button = document.querySelector("button"); //show image button
+const authorSpan = document.querySelector(".author"); //image credit
+const imgDiv = document.querySelector(".image-container"); //image container
+const img = document.querySelector(".img"); //image
+
+const getImage = async function(){
+  const res = await fetch("https://picsum.photos/v2/list?limit=100");
+  const images = await res.json;
+  console.log(images);
+};
+
+getImage();
+
