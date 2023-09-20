e18cde3c5b680c87eeb1aa5e2eb79da26a124a7f
Megan <87830907+MeganWallace@users.noreply.github.com>
2023-09-20 00:49:01 -0400

    added function to display random image in browser and updated selectRandomImage function

--- js/script.js
+++ js/script.js
@@ -13,7 +13,15 @@ const selectRandomImage = function (images) { //f'n to select image at random in
   const randomIndex = Math.floor(Math.random() * images.length);  //selects random index value between 0 and 99
   //Math.floor rounds number down, Math.random selects random number between 0 and 1
   const randomImage = images[randomIndex]; //grabs single image at randomIndex from images array produced by getImage
-  console.log(randomImage); //logs randomImage (placeholder)
+  displayImage(randomImage); //displays random image and properties in browser
 };
 
-getImage(); //calls f'n to get random image object
+const displayImage = function(randomImage){ //f'n to display and access properties of random image object
+  const author = randomImage.author; //isolates author value from random image produced by getImage
+  const imageAddress = randomImage.download_url; //isolates url value from random image produced by getImage
+  authorSpan.innerText = author; //displays random image's author information in browser
+  img.src = imageAddress; //displays random image's url in browser
+  imgDiv.classList.remove("hide"); //removes "hide" class from image div
+};
+
+getImage(); //calls f'n to get random image object (placeholder)
